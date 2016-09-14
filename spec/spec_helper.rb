9a8d7f76d03b001be28d48eded7c0af7bc8de5a1
Masahiro Iuchi <masahiro.iuchi@gmail.com>
2016-09-15 00:23:36 +0900

    Use Code Climate.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,16 @@
 require 'simplecov'
 require 'coveralls'
+require 'codeclimate-test-reporter'
+require 'webmock/rspec'
 
-Coveralls.wear! if ENV['TRAVIS']
-
-SimpleCov.start do
-  add_filter '/spec/'
+if ENV['TRAVIS']
+  WebMock.disable_net_connect!(allow: 'codeclimate.com')
+  Coveralls.wear!
+  CodeClimate::TestReporter.start do
+    add_filter '/spec/'
+  end
+else
+  SimpleCov.start do
+    add_filter '/spec/'
+  end
 end
